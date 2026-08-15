cad005e29b16ba544cfc53290b9643df1db6689f
Ryotaro Hiraki <ryoutaro.1023@outlook.com>
2026-08-15 15:56:27 -0500

    added economist white

--- R/economist.R
+++ R/economist.R
@@ -12,6 +12,18 @@ theme_economist <- function(base_size = 10,
   )
 }
 
+theme_economist_white <- function(base_size = 11,
+                          base_family = "sans",
+                          grey_bg = TRUE,
+                          horizontal = TRUE) {
+  ggthemes::theme_economist_white(
+    base_size = base_size,
+    base_family = base_family,
+    grey_bg = grey_bg,
+    horizontal = horizontal,
+  )
+}
+
 scale_color_economist <- function(...) {
   ggthemes::scale_color_economist(...)
 }
