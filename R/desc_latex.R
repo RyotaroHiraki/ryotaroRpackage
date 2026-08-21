7b1b60f0bcfd1c94d589ef01d315ee5f30ff1e72
RyotaroHiraki <rhiraki@EDHP-DW-7080090.localdomain>
2026-08-21 11:02:56 -0500

    minor2

--- R/desc_latex.R
+++ R/desc_latex.R
@@ -18,11 +18,11 @@ desc_latex <- function(data, vars = NULL, digits = 2) {
 
   desc <- data.frame(
     Variable = vars,
-    Mean = unname(data[vars], function(x) mean(x, na.rm = TRUE)),
-    SD = unname(data[vars], function(x) stats::sd(x, na.rm = TRUE)),
-    Min = unname(data[vars], function(x) min(x, na.rm = TRUE)),
-    Max = unname(data[vars], function(x) max(x, na.rm = TRUE)),
-    N = unname(data[vars], function(x) sum(!is.na(x)))
+    Mean = sapply(data[vars], function(x) mean(x, na.rm = TRUE)),
+    SD = sapply(data[vars], function(x) stats::sd(x, na.rm = TRUE)),
+    Min = sapply(data[vars], function(x) min(x, na.rm = TRUE)),
+    Max = sapply(data[vars], function(x) max(x, na.rm = TRUE)),
+    N = sapply(data[vars], function(x) sum(!is.na(x)))
   )
 
   desc[, c("Mean", "SD", "Min", "Max")] <- round(
